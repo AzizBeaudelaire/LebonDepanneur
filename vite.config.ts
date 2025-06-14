204e4e66754132fe41548f67c4f7b933c55e9c17
AzizBeaudelaire <hugo.mehdi197@gmail.com>
2025-06-14 18:40:54 +0200

    (add) modif

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Chemin relatif adapté à OVH
   server: {
     port: 5173,
     host: true,
